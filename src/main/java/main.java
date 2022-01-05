720c21b2d2f73a15e8621d9387f7d5740e57ee82
hogypitersan <hogypitersan@gmail.com>
2022-01-05 08:26:42 +0700

    commit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,3 @@
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -13,6 +12,10 @@ import java.util.*;
 
 public class main {
     private static final String HMAC_SHA512 = "HmacSHA512";
+//    private static final String URL = "http://ftp.sofcograha.co.id:3005/gajiid-API-new-test";
+    private static final String URL = "http://localhost:8080/gaji-v2";
+    private static final String APIKEY = "AFF2762EC99D4D1FB66FA679A19F4614";
+    private static final String APIID = "dfdc06db-8090-41b6-b72d-9afa9903ce03";
 
     public static void main(String[] args) {
         run();
@@ -28,7 +31,7 @@ public class main {
             String pilih = reader.readLine();
             switch(pilih) {
                 case "1":
-                    getDatakaryawan();
+                    getDatakaryawan(reader);
                     break;
                 case "2":
                     rekapPenghasilanKaryawan(reader);
@@ -49,11 +52,111 @@ public class main {
         }
     }
 
-    private static void getDatakaryawan() {
+    private static void getDatakaryawan(BufferedReader reader) {
         try {
+            String params = "";
+
+
+            System.out.print("Id karyawan: ");
+            String idKaryawan = reader.readLine();
+            System.out.print("Nama karyawan: ");
+            String namaKaryawan = reader.readLine();
+            System.out.print("Nama lokasi kerja: ");
+            String namaLokasiKerja = reader.readLine();
+            System.out.print("Nama status kepegawaian: ");
+            String namaStatusKepegawaian = reader.readLine();
+            System.out.print("Nama departemen: ");
+            String namaDepartemen = reader.readLine();
+            System.out.print("Nama jabatan: ");
+            String namaJabatan = reader.readLine();
+            System.out.print("Nama level jabatan: ");
+            String namaLeveljabatan = reader.readLine();
+            System.out.print("Nama kelompok payroll: ");
+            String namaKelompokPayroll = reader.readLine();
+            System.out.print("Status bekerja: ");
+            String statusBekerja = reader.readLine();
+            System.out.print("Nilai atribut tambahan 1: ");
+            String nilaiAtributTambahan1 = reader.readLine();
+            System.out.print("Nilai atribut tambahan 2: ");
+            String nilaiAtributTambahan2 = reader.readLine();
+            System.out.print("Nilai atribut tambahan 3: ");
+            String nilaiAtributTambahan3 = reader.readLine();
+            System.out.print("Nilai atribut tambahan 4: ");
+            String nilaiAtributTambahan4 = reader.readLine();
+            System.out.print("Nilai atribut tambahan 5: ");
+            String nilaiAtributTambahan5 = reader.readLine();
+            System.out.print("Nilai atribut tambahan 6: ");
+            String nilaiAtributTambahan6 = reader.readLine();
+            System.out.print("Nilai atribut tambahan 7: ");
+            String nilaiAtributTambahan7 = reader.readLine();
+            System.out.print("Nilai atribut tambahan 8: ");
+            String nilaiAtributTambahan8 = reader.readLine();
+            System.out.print("Nilai atribut tambahan 9: ");
+            String nilaiAtributTambahan9 = reader.readLine();
+            System.out.print("Nilai atribut tambahan 10: ");
+            String nilaiAtributTambahan10 = reader.readLine();
+
+            if (!idKaryawan.equals("")) {
+                params = params + "idKaryawan=" + idKaryawan + "&";
+            }
+            if (!namaKaryawan.equals("")) {
+                params = params + "namaKaryawan=" + namaKaryawan + "&";
+            }
+            if (!namaLokasiKerja.equals("")) {
+                params = params + "namaLokasiKerja=" + namaLokasiKerja + "&";
+            }
+            if (!namaStatusKepegawaian.equals("")) {
+                params = params + "namaStatusKepegawaian=" + namaStatusKepegawaian + "&";
+            }
+            if (!namaDepartemen.equals("")) {
+                params = params + "namaDepartemen=" + namaDepartemen + "&";
+            }
+            if (!namaJabatan.equals("")) {
+                params = params + "namaJabatan=" + namaJabatan + "&";
+            }
+            if (!namaLeveljabatan.equals("")) {
+                params = params + "namaLevelJabatan=" + namaLeveljabatan + "&";
+            }
+            if (!namaKelompokPayroll.equals("")) {
+                params = params + "namaKelompokPayroll=" + namaKelompokPayroll + "&";
+            }
+            if (!statusBekerja.equals("")) {
+                params = params + "statusBekerja=" + statusBekerja + "&";
+            }
+            if (!nilaiAtributTambahan1.equals("")) {
+                params = params + "nilaiAtributTambahan1=" + nilaiAtributTambahan1 + "&";
+            }
+            if (!nilaiAtributTambahan2.equals("")) {
+                params = params + "nilaiAtributTambahan2=" + nilaiAtributTambahan2 + "&";
+            }
+            if (!nilaiAtributTambahan3.equals("")) {
+                params = params + "nilaiAtributTambahan3=" + nilaiAtributTambahan3 + "&";
+            }
+            if (!nilaiAtributTambahan4.equals("")) {
+                params = params + "nilaiAtributTambahan4=" + nilaiAtributTambahan4 + "&";
+            }
+            if (!nilaiAtributTambahan5.equals("")) {
+                params = params + "nilaiAtributTambahan5=" + nilaiAtributTambahan5 + "&";
+            }
+            if (!nilaiAtributTambahan6.equals("")) {
+                params = params + "nilaiAtributTambahan6=" + nilaiAtributTambahan6 + "&";
+            }
+            if (!nilaiAtributTambahan7.equals("")) {
+                params = params + "nilaiAtributTambahan7=" + nilaiAtributTambahan7 + "&";
+            }
+            if (!nilaiAtributTambahan8.equals("")) {
+                params = params + "nilaiAtributTambahan8=" + nilaiAtributTambahan8 + "&";
+            }
+            if (!nilaiAtributTambahan9.equals("")) {
+                params = params + "nilaiAtributTambahan9=" + nilaiAtributTambahan9 + "&";
+            }
+            if (!nilaiAtributTambahan10.equals("")) {
+                params = params + "nilaiAtributTambahan10=" + nilaiAtributTambahan10 + "&";
+            }
+
             Date date = new Date();
-            final String uri = "http://localhost:8080/gaji-v2/open-api/karyawan/search";
-            final String apiKey = "F0D0F4AC734A42208567DA68B5575E1E"; //DB Dev - C0000000
+            final String uri = URL + "/open-api/karyawan/search?" + params;
+            final String apiKey = APIKEY;
             final String method = "GET";
             final String contentType = "application/json";
             final long currentMillisecond = date.getTime();
@@ -69,7 +172,7 @@ public class main {
             RestTemplate restTemplate = new RestTemplate();
             final HttpHeaders headers = new HttpHeaders();
             headers.set("Content-Type", "application/json");
-            headers.set("Api-ID", "d78efc7b-d30a-472b-b6b0-7ace47940900");
+            headers.set("Api-ID", APIID);
             headers.set("Api-Key", resultToken);
             headers.set("Api-Auth-Time", String.valueOf(currentMillisecond));
 
@@ -98,9 +201,8 @@ public class main {
             String params = "tahunBulan="+tahunBulan+"&pengelompokan1="+pengelompokan1+"&nilaiPengelompokan1="+nilaiPengelompokan1+"&pengelompokan2="+pengelompokan2+"&nilaiPengelompokan2="+nilaiPengelompokan2;
 
             Date date = new Date();
-            final String uri = "http://localhost:8080/gaji-v2/open-api/pendapatanPotongan/rekapBulanan?"+params;
-            final String apiKey = "CA4C4E6C550E41059BAB2AD020D2A79A"; //DB Dev - C0000014
-//            final String apiKey = "F0D0F4AC734A42208567DA68B5575E1E"; //DB Dev - C0000000
+            final String uri = URL + "/open-api/pendapatanPotongan/rekapBulanan?"+params;
+            final String apiKey = APIKEY;
             final String method = "GET";
             final String contentType = "application/json";
             final long currentMillisecond = date.getTime();
@@ -116,8 +218,7 @@ public class main {
             RestTemplate restTemplate = new RestTemplate();
             final HttpHeaders headers = new HttpHeaders();
             headers.set("Content-Type", "application/json");
-            headers.set("Api-ID", "f989a3fd-599b-4746-acc0-c413af0f808c"); //DB Dev - C0000014
-//            headers.set("Api-ID", "d78efc7b-d30a-472b-b6b0-7ace47940900"); //DB Dev - C0000000
+            headers.set("Api-ID", APIID);
             headers.set("Api-Key", resultToken);
             headers.set("Api-Auth-Time", String.valueOf(currentMillisecond));
 
@@ -165,8 +266,8 @@ public class main {
             bodyList.add(body);
 
             Date date = new Date();
-            final String uri = "http://localhost:8080/gaji-v2/open-api/pendapatanPotongan";
-            final String apiKey = "F0D0F4AC734A42208567DA68B5575E1E"; //DB Dev - C0000000
+            final String uri = URL + "/open-api/pendapatanPotongan";
+            final String apiKey = APIKEY;
             final String method = "POST";
             final String contentType = "application/json";
             final long currentMillisecond = date.getTime();
@@ -182,7 +283,7 @@ public class main {
             RestTemplate restTemplate = new RestTemplate();
             final HttpHeaders headers = new HttpHeaders();
             headers.set("Content-Type", "application/json");
-            headers.set("Api-ID", "d78efc7b-d30a-472b-b6b0-7ace47940900");
+            headers.set("Api-ID", APIID);
             headers.set("Api-Key", resultToken);
             headers.set("Api-Auth-Time", String.valueOf(currentMillisecond));
 
